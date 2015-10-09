e39c982eeae38dcea9ddba7e2e2175df13a80944
Ronnie Roberts <ronnie.roberts@purered.net>
2015-10-09 07:54:52 -0400

    edit Litmus credentials

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,9 +30,9 @@ module.exports = function(grunt) {
       test: {
         src: ['prod/'+grunt.option('template')],
         options: {
-          username: 'billing@luckiemail.com',
-          password: 'Luckie!@#',
-          url: 'https://luckie.litmus.com',
+          username: '',
+          password: '',
+          url: 'https://purered.litmus.com',
           clients: ['ol2000','ol2002','ol2003','ol2007','ol2010','ol2011','ol2013','android4','androidgmailapp','ipad','iphone4','iphone5','iphone5s','iphone6','iphone6plus','chromegmailnew', 'ffgmailnew','gmailnew','chromeoutlookcom','outlookcom','ffoutlookcom']
         }
       }
