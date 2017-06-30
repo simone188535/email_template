c91fb99f07c48d4a8b05da88625a4e6425e42993
Richard Hines <richard.hines@purered.net>
2017-06-29 21:07:19 -0400

    KF Updates for better erb handling

--- templates/KF/KF.py
+++ templates/KF/KF.py
@@ -40,71 +40,36 @@ class KFLoyalty(emailBuilder):
         self.layoutSheetName = "Layout"
         self.layoutsheet = self.workbook.sheet_by_name(self.layoutSheetName)
 
-    def generateImage(self, *images):
-        if len(images) == 5:
-            type = "image-5-columns"
-        elif len(images) == 4:
-            type = "image-4-columns"
-        elif len(images) == 3:
-            type = "image-3-columns"
-        elif len(images) == 2:
-            type = "image-2-columns"
-        else:
-            type = "image"
+    def generateImage(self, images):
+        articleType = "table"
 
-        self.contents.insert(self.insertRow(), "- type: '" + type + "'" + self.newLine)
+        table = []
 
-        row1 = self.findImageRow(images[0])
-        path, imageName = self.getImageRelativePathAndName(images[0])
-        self.contents.insert(self.insertRow(), "  img_link: '" + self.getImageLink(row1[self.linkColumn]) + "'" + self.newLine)
-        self.contents.insert(self.insertRow(), "  img_alt: '" + self.encodeText(row1[self.altTextColumn]) + "'" + self.newLine)
-        self.contents.insert(self.insertRow(), "  img_url: '" + path + imageName + "'" + self.newLine)
+        for image in images:
+            imgData = ['']*6
 
-        if len(images) > 1:
-            row2 = self.findImageRow(images[1])
-            path, imageName = self.getImageRelativePathAndName(images[1])
-            self.contents.insert(self.insertRow(), "  img_2_link: '" + self.getImageLink(row2[self.linkColumn]) + "'" + self.newLine)
-            self.contents.insert(self.insertRow(), "  img_2_alt: '" + self.encodeText(row2[self.altTextColumn]) + "'" + self.newLine)
-            self.contents.insert(self.insertRow(), "  img_2_url: '" + path + imageName + "'" + self.newLine)
+            row = self.findImageRow(image)
 
-        if len(images) > 2:
-            row3 = self.findImageRow(images[2])
-            path, imageName = self.getImageRelativePathAndName(images[2])
-            self.contents.insert(self.insertRow(), "  img_3_link: '" + self.getImageLink(row3[self.linkColumn]) + "'" + self.newLine)
-            self.contents.insert(self.insertRow(), "  img_3_alt: '" + self.encodeText(row3[self.altTextColumn]) + "'" + self.newLine)
-            self.contents.insert(self.insertRow(), "  img_3_url: '" + path + imageName + "'" + self.newLine)
+            path, imageName = self.getImageRelativePathAndName(image)
 
-        if len(images) > 3:
-            row4 = self.findImageRow(images[3])
-            path, imageName = self.getImageRelativePathAndName(images[3])
-            self.contents.insert(self.insertRow(), "  img_4_link: '" + self.getImageLink(row4[self.linkColumn]) + "'" + self.newLine)
-            self.contents.insert(self.insertRow(), "  img_4_alt: '" + self.encodeText(row4[self.altTextColumn]) + "'" + self.newLine)
-            self.contents.insert(self.insertRow(), "  img_4_url: '" + path + imageName + "'" + self.newLine)
+            imgData[0] = "image"
+            imgData[1] = str(path + imageName).encode('ascii','ignore')
+            imgData[2] = self.getImageLink(row[self.linkColumn]).encode('ascii','ignore')
+            imgData[5] = row[self.altTextColumn].encode('ascii','ignore')
 
-        if len(images) > 4:
-            row5 = self.findImageRow(images[4])
-            path, imageName = self.getImageRelativePathAndName(images[4])
-            self.contents.insert(self.insertRow(), "  img_5_link: '" + self.getImageLink(row5[self.linkColumn]) + "'" + self.newLine)
-            self.contents.insert(self.insertRow(), "  img_5_alt: '" + self.encodeText(row5[self.altTextColumn]) + "'" + self.newLine)
-            self.contents.insert(self.insertRow(), "  img_5_url: '" + path + imageName + "'" + self.newLine)
+            imgData = self.addAdditionalFields(image, imgData)
 
+            table.append(imgData)
+
+        self.contents.insert(self.insertRow(), "- type: '" + articleType + "'" + self.newLine)
+        self.contents.insert(self.insertRow(), "  data: " + str(table) + "" + self.newLine)
         self.contents.insert(self.insertRow(), self.newLine)
 
     def generateYmlContent(self):
         for irow in self.layoutsheet.rows():
             while '' in irow:
                 irow.remove('')
-
-            if len(irow) == 1:
-                self.generateImage(irow[0])
-            elif len(irow) == 2:
-                self.generateImage(irow[0], irow[1])
-            elif len(irow) == 3:
-                self.generateImage(irow[0], irow[1], irow[2])
-            elif len(irow) == 4:
-                self.generateImage(irow[0], irow[1], irow[2], irow[3])
-            elif len(irow) == 5:
-                self.generateImage(irow[0], irow[1], irow[2], irow[3], irow[4])
+            self.generateImage(irow)
 
 
 class KFGeneric(KFLoyalty):
