69bfafc50b6a49cc263e4d404f31b07abda4344f
Shannon Chopson <schopson@PR1-DM-Dev03.local>
2018-09-14 13:25:24 -0400

    Work on OTP template 091418; added OTP folder and copied genericEmail to use as base for OTPBuilder

---
+++ templates/OTP/OTP.py
@@ -0,0 +1,14 @@
+from emailBuildClass import emailBuilder
+
+class OTPBuilder(emailBuilder):
+    def __init__(self, crf_file, source_images, emailName):
+        self.projectNamePrefix = "OTP_"
+        self.folderPath = "otp"
+        self.ymlTemplate = "OTP/OTP_Template_053017.yml"
+        self.erbTemplate = "OTP/OTP_Template_053017.erb"
+
+        self.sheet = raw_input("Enter Excel Sheet Name with Email Data\n")
+        self.altTextColumn = raw_input("Enter Excel Column with Alt Text (Letter or Index Number)\n")
+        self.linkColumn = raw_input("Enter Excel Column with URL (Letter or Index Number)\n")
+
+        super(genericEmailBuilder, self).__init__(crf_file, source_images, emailName)
