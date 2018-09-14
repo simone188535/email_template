a45b20f9bdf37dd41839c996bd7d48aabdf44155
Shannon Chopson <schopson@PR1-DM-Dev03.local>
2018-09-14 17:20:30 -0400

    Continuing to build the OTP script; added most recent email as template files but they are not true templates yet.

--- templates/OTP/OTP.py
+++ templates/OTP/OTP.py
@@ -1,5 +1,4 @@
 from distutils.dir_util import copy_tree
-from shutil import copyfile
 import pyexcel as p
 from emailBuildClass import emailBuilder
 # Needed for automatically sending tests in Mailchimp
@@ -10,11 +9,43 @@ class OTPBuilder(emailBuilder):
     def __init__(self, crf_file, source_images, emailName):
         self.projectNamePrefix = "OTP_"
         self.folderPath = "otp"
-        self.ymlTemplate = "OTP/OTP_Template_053017.yml"
-        self.erbTemplate = "OTP/OTP_Template_053017.erb"
+        self.ymlTemplate = "OTP/OTP_Template_091418.yml"
+        self.erbTemplate = "OTP/OTP_Template_091418.erb"
 
-        self.sheet = raw_input("Enter Excel Sheet Name with Email Data\n")
-        self.altTextColumn = raw_input("Enter Excel Column with Alt Text (Letter or Index Number)\n")
-        self.linkColumn = raw_input("Enter Excel Column with URL (Letter or Index Number)\n")
+        self.sheetName = 0
+        self.altTextColumn = 2 # Column numbers starting from 0
+        self.linkColumn = 5
 
-        super(genericEmailBuilder, self).__init__(crf_file, source_images, emailName)
+        super(OTPBuilder, self).__init__(**kwargs)
+
+    def update(self):
+        self.updateHeader()
+
+	def updateHeader(self):
+        # find and update title
+        nameFound = False
+        titleFound = False
+
+        for irow in self.sheet.rows():
+            for x in range(0, len(irow) -1):
+                if irow[x].strip() == "Task:":
+                    self.fileContents = self.fileContents.replace("__EMAIL_NAME__", irow[x+1])
+                    nameFound = True
+                elif irow[x].strip() == "Subject Line:":
+                    self.fileContents = self.fileContents.replace("__TITLE__", irow[x+1])
+                    titleFound = True
+                if nameFound and titleFound:
+                    break
+            if nameFound and titleFound:
+                break
+
+        # More work needed here...See (defunct) bbB.py file for an example
+
+
+    def loadData(self):
+        super(KFLoyalty, self).loadData()
+        self.layoutSheetName = "Layout"
+        self.layoutsheet = self.workbook.sheet_by_name(self.layoutSheetName)
+
+    def generateYmlContent(self):
+    	# Do we need to hook into this to adjust how the images are found and pulled in? See KF.py for reference.
